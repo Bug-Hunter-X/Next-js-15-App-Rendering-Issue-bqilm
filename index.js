78ccbce552bb62d79b380e0779d04d246390192f
BugHunter-X <askzenai@gmail.com>
2025-01-06 08:48:04 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Next.js 15 App</h1>
+      <p>This is a simple Next.js 15 app.</p>
+    </div>
+  );
+}
+```
